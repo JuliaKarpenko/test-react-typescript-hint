c9aa32ea690a3729c733dc38ea27c9e3970e3a7a
J.Karpenko <Jule4ka.jd@gmail.com>
2024-04-29 16:08:36 +0300

    add gh page

--- src/components/Hint/tests/hint.test.tsx
+++ src/components/Hint/tests/hint.test.tsx
@@ -1,97 +1,93 @@
-import { render, screen } from '@testing-library/react'
-import Hint from '../Hint';
-import userEvent from '@testing-library/user-event'
-import { useEffect, useRef, useCallback } from 'react';
-import { Side } from '../hooks/useHint';
+import { render, screen } from "@testing-library/react";
+import Hint from "../Hint";
+import userEvent from "@testing-library/user-event";
+import { useEffect, useRef, useCallback } from "react";
+import { Side } from "../hooks/useHint";
 
 test("Hint component renders successfully", () => {
-    render(
-        <Hint
-            hintContent="I'm a hint"
-            side="top"
-        >
-            <div>Test children</div>
-        </Hint>
-    );
+	render(
+		<Hint hintContent="I'm a hint" side="top">
+			<div>Test children</div>
+		</Hint>
+	);
 
-    const childrenElement = screen.getByText(/Test children/i);
+	const childrenElement = screen.getByText(/Test children/i);
 
-    expect(childrenElement).toBeInTheDocument();
+	expect(childrenElement).toBeInTheDocument();
 });
 
 test("Hint displays correctly when visible", () => {
-    const mockHintContent = "This is a hint";
-    const mockSide: Side = "top";
-  
-    render(
-      <Hint hintContent={mockHintContent} side={mockSide}>
-        Hover me
-      </Hint>
-    );
-  
-    const hoverMeButton = screen.getByText(/hover me/i);
-    userEvent.hover(hoverMeButton);
-  
-    const hintContent = screen.getByText(mockHintContent);
-    expect(hintContent).toBeInTheDocument();
-  });
-  
-  test("Hint does not display when not visible", () => {
-    const mockHintContent = "This is a hint";
-    const mockSide: Side = "top";
-  
-    render(
-      <Hint hintContent={mockHintContent} side={mockSide}>
-        Hover me
-      </Hint>
-    );
-  
-    const hintContent = screen.queryByText(mockHintContent);
-    expect(hintContent).not.toBeInTheDocument();
-  });
+	const mockHintContent = "This is a hint";
+	const mockSide: Side = "top";
 
+	render(
+		<Hint hintContent={mockHintContent} side={mockSide}>
+			Hover me
+		</Hint>
+	);
+
+	const hoverMeButton = screen.getByText(/hover me/i);
+	userEvent.hover(hoverMeButton);
+
+	const hintContent = screen.getByText(mockHintContent);
+	expect(hintContent).toBeInTheDocument();
+});
+
+test("Hint does not display when not visible", () => {
+	const mockHintContent = "This is a hint";
+	const mockSide: Side = "top";
+
+	render(
+		<Hint hintContent={mockHintContent} side={mockSide}>
+			Hover me
+		</Hint>
+	);
+
+	const hintContent = screen.queryByText(mockHintContent);
+	expect(hintContent).not.toBeInTheDocument();
+});
 
 const TestComponent = () => {
-    const hintContainerRef = useRef<HTMLDivElement>(null);
-    const onMouseOver = useCallback(() => {
-      console.log('Mouse over');
-    }, []);
-  
-    const onMouseOut = useCallback(() => {
-        console.log('Mouse out');
-      }, []);
+	const hintContainerRef = useRef<HTMLDivElement>(null);
+	const onMouseOver = useCallback(() => {
+		console.log("Mouse over");
+	}, []);
 
-    useEffect(() => {
-      const element = hintContainerRef?.current;
-  
-      if (element) {
-        element.addEventListener("mouseenter", onMouseOver);
-        element.addEventListener("mouseleave", onMouseOut);
-      }
-  
-      return () => {
-        if (element) {
-          element.removeEventListener("mouseenter", onMouseOver);
-          element.removeEventListener("mouseleave", onMouseOut);
-        }
-      };
-    }, [onMouseOver]);
-  
-    return <div ref={hintContainerRef}>Hover me</div>;
-  };
+	const onMouseOut = useCallback(() => {
+		console.log("Mouse out");
+	}, []);
 
-  test("Mouse over event handler called", () => {
-    const spy = jest.spyOn(console, 'log');
-    render(<TestComponent />);
-    const element = screen.getByText(/Hover me/i);
-    userEvent.hover(element);
-    expect(spy).toHaveBeenCalledWith("Mouse over");
-  });
+	useEffect(() => {
+		const element = hintContainerRef?.current;
 
-  test("Mouse out event handler called", () => {
-    const spy = jest.spyOn(console, 'log');
-    render(<TestComponent />);
-    const element = screen.getByText(/Hover me/i);
-    userEvent.unhover(element);
-    expect(spy).toHaveBeenCalledWith("Mouse out");
-  });
+		if (element) {
+			element.addEventListener("mouseenter", onMouseOver);
+			element.addEventListener("mouseleave", onMouseOut);
+		}
+
+		return () => {
+			if (element) {
+				element.removeEventListener("mouseenter", onMouseOver);
+				element.removeEventListener("mouseleave", onMouseOut);
+			}
+		};
+	}, [onMouseOver]);
+
+	return <div ref={hintContainerRef}>Hover me</div>;
+};
+
+test("Mouse over event handler called", () => {
+	const spy = jest.spyOn(console, "log");
+	render(<TestComponent />);
+	const element = screen.getByText(/Hover me/i);
+	userEvent.hover(element);
+	expect(spy).toHaveBeenCalledWith("Mouse over");
+});
+
+test("Mouse out event handler called", () => {
+	const spy = jest.spyOn(console, "log");
+	render(<TestComponent />);
+	const element = screen.getByText(/Hover me/i);
+	userEvent.unhover(element);
+	expect(spy).toHaveBeenCalledWith("Mouse out");
+});
